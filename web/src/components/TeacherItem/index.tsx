807e8502b65013a68e9e9e08611bfe58f29f37f8
pablojr17 <pablojr17@gmail.com>
2020-08-07 13:39:08 -0300

    🦄️ A escolha da stack

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,33 +2,54 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
         <header>
-            <img src="https://avatars2.githubusercontent.com/u/47211806?s=460&u=38f26b994d8a53b35eeb226ed16893d1a9b659f2&v=4" alt="Pablo Gomes"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
              <div>
-                 <strong>Pablo Gomes</strong>
-                 <span>Quimica</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
              </div>
         </header>
 
-        <p>
-            Estusiasta das melhhores tecnologias de quimica avançada.
-             <br/><br/>
-             Apaixonado por explodir coias em laboratórios e por mudar a vida das pessoas atráves de experiencias.
-        </p>
+        <p>{teacher.bio}</p>
 
         <footer>
             <p>
                 Preço/hora
-                <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a 
+                target="_blank"
+                onClick={createNewConnection} 
+                href={`https://wa.me/${teacher.whatsapp}`}
+            >
                 <img src={whatsappIcon} alt="Whatsapp"/>
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>
     )
